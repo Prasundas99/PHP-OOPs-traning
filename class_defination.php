bb9bceb153f02e76e1207019afd3ed2e572559eb
Prasundas99 <prasundas115@gmail.com>
2020-11-21 00:43:27 +0530

    completed methods

--- class_defination.php
+++ class_defination.php
@@ -1,15 +1,39 @@
 <?php
+//A Class is a user defined data-type which has data members and member functions
 //class Classname(starts with capital)
 class Cars {
 //Class defination
 
+    //method are function declared in  class
+    function greeting(){
+    //function is blueprint of an object
+
+    }
+
+        function greeting2(){
+    //function is blueprint of an object
+
+    }
 
 }
 
-$my_classes = get_declared_classes();
 
+//php keeps track of methods
+
+$the_methods = get_class_methods('Cars');//have to give parameters
+foreach ($the_methods as $method) {
+    echo $method ." <br>";
+}
+
+
+
+
+//php keeps tracks of class
+/*$my_classes = get_declared_classes();
 foreach ($my_classes as $class) {
     echo $class ." <br>";
-}
+}*/
 
 ?>
+
+
