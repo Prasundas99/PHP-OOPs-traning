56e632c7502ff9b9a157499da86a3cab88aaf09c
Prasundas99 <prasundas115@gmail.com>
2021-09-04 21:54:51 +0530

    PHP cms project added

---
+++ PHP CMS Project/modules/page/models/Page.php
@@ -0,0 +1,17 @@
+<?php
+namespace modules\page\models;
+class Page extends \src\Entity {
+    
+    public function __construct($dbc) {
+        parent::__construct($dbc, 'pages');
+    }
+    
+    protected function initFields() {
+       $this->fields = [
+            'id',
+            'title',
+            'content'
+        ];
+    }
+    
+}
